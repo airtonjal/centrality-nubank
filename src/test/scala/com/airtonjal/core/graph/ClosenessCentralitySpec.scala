5a7c7e9f4e5820328cf0545606a1c7815bcb2df8
airton.liborio <airton.liborio@webradar.com>
2015-06-24 19:05:00 -0300

    - Testing fraud detection and edge adding

--- src/test/scala/com/airtonjal/core/graph/ClosenessCentralitySpec.scala
+++ src/test/scala/com/airtonjal/core/graph/ClosenessCentralitySpec.scala
@@ -10,33 +10,33 @@ class ClosenessCentralitySpec extends FlatSpec with Matchers {
 
   val edges = Seq(Edge(4, 5), Edge(1, 2), Edge(2, 3))
   val graph = GraphBuilder.createGraph(edges)
-  var scores = new BreadthFirstSearch().farnessScores(graph)
+  var scores = new BFS().farnessScores(graph)
 
   "Closeness algorithm" should "calculate scores correctly" in {
     var scoreMap = scores.map(s => s.vertex -> s.score).toMap
 
-    scoreMap(1) should be (3.0)
-    scoreMap(2) should be (2.0)
-    scoreMap(3) should be (3.0)
-    scoreMap(4) should be (1.0)
-    scoreMap(5) should be (1.0)
+    scoreMap(1) should be (3)
+    scoreMap(2) should be (2)
+    scoreMap(3) should be (3)
+    scoreMap(4) should be (1)
+    scoreMap(5) should be (1)
 
-    scores = new BreadthFirstSearch().farnessScores(graph + Edge(1, 3))
+    scores = new BFS().farnessScores(graph + Edge(1, 3))
     scoreMap = scores.map(s => s.vertex -> s.score).toMap
 
-    scoreMap(1) should be (2.0)
-    scoreMap(2) should be (2.0)
-    scoreMap(3) should be (2.0)
-    scoreMap(4) should be (1.0)
-    scoreMap(5) should be (1.0)
+    scoreMap(1) should be (2)
+    scoreMap(2) should be (2)
+    scoreMap(3) should be (2)
+    scoreMap(4) should be (1)
+    scoreMap(5) should be (1)
   }
 
   "Closeness algorithm" should "calculate farness correctly" in {
-    val farness1 = new BreadthFirstSearch().farness(graph, 1)
-    val farness2 = new BreadthFirstSearch().farness(graph, 2)
-    val farness3 = new BreadthFirstSearch().farness(graph, 3)
-    val farness4 = new BreadthFirstSearch().farness(graph, 4)
-    val farness5 = new BreadthFirstSearch().farness(graph, 5)
+    val farness1 = new BFS().farness(graph, 1)
+    val farness2 = new BFS().farness(graph, 2)
+    val farness3 = new BFS().farness(graph, 3)
+    val farness4 = new BFS().farness(graph, 4)
+    val farness5 = new BFS().farness(graph, 5)
 
     farness1 should be (3)
     farness2 should be (2)
@@ -46,17 +46,59 @@ class ClosenessCentralitySpec extends FlatSpec with Matchers {
   }
 
   "Closeness algorithm" should "calculate closeness correctly" in {
-    val closeness1 = new BreadthFirstSearch().closeness(graph, 1)
-    val closeness2 = new BreadthFirstSearch().closeness(graph, 2)
-    val closeness3 = new BreadthFirstSearch().closeness(graph, 3)
-    val closeness4 = new BreadthFirstSearch().closeness(graph, 4)
-    val closeness5 = new BreadthFirstSearch().closeness(graph, 5)
+    val closeness1 = new BFS().closeness(graph, 1)
+    val closeness2 = new BFS().closeness(graph, 2)
+    val closeness3 = new BFS().closeness(graph, 3)
+    val closeness4 = new BFS().closeness(graph, 4)
+    val closeness5 = new BFS().closeness(graph, 5)
 
     closeness1 should be (1f/3)
     closeness2 should be (1f/2)
     closeness3 should be (1f/3)
     closeness4 should be (1f/1)
     closeness5 should be (1f/1)
+  }
+
+  var bfsWithState = new BFSWithState(graph)
+  bfsWithState.calculateScores()
+
+  "BFSWithState" should "recalculate closeness after adding edge" in {
+    var scoreMap = bfsWithState.scores.map(s => s.vertex -> s.score).toMap
+
+    scoreMap(1) should be (1f / 3)
+    scoreMap(2) should be (1f / 2)
+    scoreMap(3) should be (1f / 3)
+    scoreMap(4) should be (1f / 1)
+    scoreMap(5) should be (1f / 1)
+
+    bfsWithState + Edge(1, 3)
+    scoreMap = bfsWithState.scores.map(s => s.vertex -> s.score).toMap
+
+    scoreMap(1) should be (1f / 2)
+    scoreMap(2) should be (1f / 2)
+    scoreMap(3) should be (1f / 2)
+    scoreMap(4) should be (1f / 1)
+    scoreMap(5) should be (1f / 1)
+
+    bfsWithState + Edge(1, 4)
+    scoreMap = bfsWithState.scores.map(s => s.vertex -> s.score).toMap
+
+    scoreMap(1) should be (1f / 5)
+    scoreMap(2) should be (1f / 7)
+    scoreMap(3) should be (1f / 7)
+    scoreMap(4) should be (1f / 6)
+    scoreMap(5) should be (1f / 9)
+  }
+
+  "BFSWithState" should "recalculate closeness after node is marked as fraudulent" in {
+    bfsWithState.fraud(1)
+    val scoreMap = bfsWithState.scores.map(s => s.vertex -> s.score).toMap
+
+    scoreMap(1) should be (0f)
+    scoreMap(2) should be (1f / 14)
+    scoreMap(3) should be (1f / 14)
+    scoreMap(4) should be (1f / 12)
+    scoreMap(5) should be (3f / 36)
   }
 
 }
