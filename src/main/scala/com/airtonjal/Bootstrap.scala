cf6f6696a4928631f21da06731562cff2c2e41e2
airton.liborio <airton.liborio@webradar.com>
2015-06-25 16:05:54 -0300

    - Moving edges.txt loading to GraphService

--- src/main/scala/com/airtonjal/Bootstrap.scala
+++ src/main/scala/com/airtonjal/Bootstrap.scala
@@ -4,7 +4,7 @@ import akka.actor.{ActorSystem, Props}
 import akka.event.Logging
 import akka.io.IO
 import com.airtonjal.core.graph.{Edge, GraphBuilder}
-import com.airtonjal.service.GraphService
+import com.airtonjal.service.GraphServiceImpl
 import spray.can.Http
 
 import scala.io.Source
@@ -15,16 +15,11 @@ import scala.io.Source
  */
 object Bootstrap extends App {
 
-  val FILENAME = "edges.txt"
-  val stream = Source.fromURL(getClass().getResource("/" + FILENAME))
-  val edgeSeq = stream.getLines.toSeq.map(line => line.split(" +")).map(v => Edge(v(0).toInt, v(1).toInt))
-  val graph = GraphBuilder.createGraph(edgeSeq)
-
   implicit val system = ActorSystem("graph-REST-service")
   val log = Logging(system, getClass)
 
   // Our service implementation
-  val graphService = system.actorOf(Props(new GraphService(graph)), "graph-service")
+  val graphService = system.actorOf(Props[GraphServiceImpl], "graph-service")
 
   // Starts http server, hardcoded properties only for the sake of demonstration
   IO(Http) ! Http.Bind(graphService, interface = "127.0.0.1", port = 8080)
--- src/main/scala/com/airtonjal/service/GraphService.scala
+++ src/main/scala/com/airtonjal/service/GraphService.scala
@@ -1,33 +1,40 @@
 package com.airtonjal.service
 
 import akka.actor.{Actor, ActorLogging, ActorRefFactory}
-import com.airtonjal.core.graph.{BFSWithState, Edge, Graph}
+import com.airtonjal.core.graph.{GraphBuilder, BFSWithState, Edge}
 import org.json4s.{DefaultFormats, Formats}
 import spray.httpx.Json4sSupport
 import spray.routing.{ExceptionHandler, HttpService, RejectionHandler, RoutingSettings}
 import spray.util.LoggingContext
+
+import scala.io.Source
 
 /* Used to mix in Spray's Marshalling Support with json4s */
 object Json4sProtocol extends Json4sSupport {
   implicit def json4sFormats: Formats = DefaultFormats
 }
 
+class GraphServiceImpl extends GraphService {
+  override def receive: Receive = runRoute(graphRoute)(ExceptionHandler.default, RejectionHandler.Default, context,
+    RoutingSettings.default, LoggingContext.fromActorRefFactory)
+
+  def actorRefFactory: ActorRefFactory = context
+}
+
 /**
  * REST graph service
  * @author <a href="mailto:airtonjal@gmail.com">Airton Libório</a>
  */
-class GraphService(var graph: Graph) extends HttpService with Actor with ActorLogging {
-  override def receive: Receive = runRoute(listRoute)(ExceptionHandler.default, RejectionHandler.Default, context,
-    RoutingSettings.default, LoggingContext.fromActorRefFactory)
-
-  def actorRefFactory: ActorRefFactory = context
+trait GraphService extends HttpService with Actor with ActorLogging {
+  val FILENAME = "edges.txt"
+  val stream = Source.fromURL(getClass().getResource("/" + FILENAME))
+  val edgeSeq = stream.getLines().toSeq.map(line => line.split(" +")).map(v => Edge(v(0).toInt, v(1).toInt))
+  val cc = new BFSWithState(GraphBuilder.createGraph(edgeSeq))
+  cc.calculateScores()
 
   import Json4sProtocol._
 
-  val cc = new BFSWithState(graph)
-  cc.calculateScores()
-
-  val listRoute = {
+  val graphRoute = {
     pathPrefix("graph") {
       path("list") {
         get {
@@ -36,15 +43,14 @@ class GraphService(var graph: Graph) extends HttpService with Actor with ActorLo
             cc.scores
           }
         }
-      } ~
-        path("add" / IntNumber / IntNumber) { (vertex1, vertex2) =>
-          post {
-            complete {
-              val edge = Edge(vertex1, vertex2)
-              log.info("Adding edge " + edge)
-              cc + edge
-              "Edge " + edge + "added succesfully"
-            }
+      } ~ path("add" / IntNumber / IntNumber) { (vertex1, vertex2) =>
+        post {
+          complete {
+            val edge = Edge(vertex1, vertex2)
+            log.info("Adding edge " + edge)
+            cc + edge
+            "Edge " + edge + "added succesfully"
+          }
         }
       } ~ path("fraud" / IntNumber) { vertex =>
         put {
